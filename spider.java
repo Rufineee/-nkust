090b88b89f50d90f0f66da84b4e9264ca7e5c839
Rufineee <93572230+Rufineee@users.noreply.github.com>
2021-12-27 21:19:17 +0800

    Add files via upload

--- spider.java
+++ spider.java
@@ -7,10 +7,9 @@ public class spider {
         for (int i = 0; i < 10; i++) {
             int page = i * 25;
             Document document = Jsoup.connect("https://movies.yahoo.com.tw/movie_intheaters.html"+ page).get();
-            Elements items = document.getElementsByClass("item");
+            Elements items = document.getElementsByClass("div.release_info");
             for (Element item : items) {
-                String num = item.getElementsByTag("em").get(0).text();
-                String title = item.getElementsByClass("hd").get(0).getElementsByTag("span").get(0).text();
+                String title = item.getElementsByClass("hd").get(0).getElementsByTag("a.gabtn").get(0).text();
             }
         }
     }
